1b0ae71e37e0add786fae46ebd42b5cc09c80213
jozuebueno <jozuedasilva@hotmail.com>
2024-02-23 16:54:08 -0300

    aula23

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ console.log(area)*/
  }
  imprimirolamundo()*/
 
+  // exercicio 2
+
  /*function iprimirnome( nome){
     console.log( "ola ", nome,)
  }
@@ -69,17 +71,121 @@ console.log(area)*/
     console.log(calculaArea(2,3))*/
 
             // exercicio 3
-     /*function somadoisnumeros(altura , largura) {
-        const area = altura + largura
-        return area
-    }
-    const areacalculada = somadoisnumeros(5 , 3)
-        console.log(calculaArea(5,3))*/
+          /*function somadoisnumeros(altura , largura) {
+            const area = altura + largura
+           return area
+          }
+         const areacalculada = somadoisnumeros(5 , 3)
+           console.log(calculaArea(5,3))*/
 
         
             // exercicio 4
-      function recebearray(arrayDenumeros = []){
-          return [(arrayDenumeros .length)/2 , (arrayDenumeros[0])/2];
-      }
+            /*function recebearray(arrayDenumeros = []){
+                return [(arrayDenumeros .length)/2 , (arrayDenumeros[0])/2];
+            }
 
-           console.log(recebearray([1,2,3,4,5,6,7,8,9]))
+           console.log(recebearray([1,2,3,4,5,6,7,8,9]))*/    
+           
+           /*const professor = {
+            nome: "vitor",
+            idade: 27,
+            tarefas:['Dar aula', 'responder duvidas'],
+            contarpiada: function() {
+            console.log('É pa ve ou pa come')
+            email:'vitor@gmail.com'
+              }
+
+             }
+             console.log(professor["idade"] );
+             console.log( professor[ 'email'])
+             console.log(professor.nome = 'jozue');
+             console.log('jozue')*/
+
+                /*const filme = {
+                nome : "uma historia de vida",
+                direcao:[' nao sei'],
+                ano:1988,
+                elenco:["neo" , " conexao", "desconexao" , "ola"],
+                visto: true,
+                }
+               console.log(filme.nome);
+               console.log( filme.direcao);
+               console.log( filme["ano"]);
+               console.log(filme["elenco"]);
+               console.log(filme["visto"]);*/
+
+                /* const pessoa = {
+                nome: "joao",
+                idade: 25,
+                generomusical: "gaucha",
+               }
+               console.log("o nome da pessoa é " , pessoa.nome, "ela tem" , pessoa.idade , "anos e gosta muito de musica" , pessoa.generomusical, );  */
+
+               /* const donodopet ={
+                nome: "vitor hugo",
+                pet: {
+                  nomedopet: "wanda",
+                  raca: "vira-lata",
+                  idade: 1
+                }
+               }
+               console,log(donodopet,pet.donodopet); */
+
+              /*  const curso = {
+                nome:" noturno frontend",
+                linguagens: [ "js" , "css" , "html"]
+
+               }
+                console.log( curso.linguagens[0]) */
+                
+                
+                /* // nome:( objeto) , modulo:(chave) , Andrei,Vitor,Mina:(valor)
+                const professores = [
+                  {nome: "Andrei" , modulo:1},   
+                  {nome: "Vitor" , modulo:2},
+                  {nome: "Mina" , modulo:3}
+                ]
+                //console.log(professores[0].nome)
+                console.log(professores[1].nome) */
+/* 
+                const curso = {
+                  nome: "frontend",
+                  linguagens:[ "js" , "css" , "html"]
+
+                }
+                     console.log("curso.numerosestudante = 50") */
+
+                 /*    const filme = {
+                nome : "uma historia de vida",
+                direcao:[' nao sei'],
+                ano:1988,
+                elenco:["xuxa" , " conexao", "desconexao" , "ola"],
+                visto: true,
+                }
+               filme.personagens= [ "personagens 1" , "personagens 2"," personagens 3", "personagens 4"];
+               console.log(filme.elenco[0] + "-" + filme.personagens[0]);
+               console.log(filme.elenco[1] + "-" + filme.personagens[1]);
+               console.log(filme.elenco[2] + "-" + filme.personagens[2]);
+               console.log(filme.elenco[3] + "-" + filme.personagens[3]);
+                filme.elenco[0] ="xuxa";
+                console.log(filme.elenco[0]);
+                console.log(filme); */
+
+             /*    const usuario = {
+                  nome: 'prof',
+                  idade: 25,
+                  email:'prof@senacrs.com.br',
+                  cidade: 'sao paulo'
+                }
+                 const novoUsuario = {
+                  ...usuario,
+                  nome: 'joao',
+                  idade:28,
+                 } */
+              
+                 const listadenomes = ["mica" , "paula" , "vitor"]
+                 const copialistadenomes = [ ...listadenomes]
+
+                 console.log(copialistadenomes) // ["mica" , "paula" , "vitor"]
+
+                 
